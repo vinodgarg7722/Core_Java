a7ce62756fb1b2196cce80478719352ca74b875a
VinodGarg <vinodgarg070702@gmail.com>
2025-03-05 10:52:05 +0530

    commit

--- Module_6/6.4(This_Keyword)/Car.java
+++ Module_6/6.4(This_Keyword)/Car.java
@@ -10,18 +10,43 @@ public class Car {
     int noOfSeats;
 
 
+    // public void start(){
+    //     if (currentFuelInLiters == 0 ){
+    //         System.out.println("Car is out of fuel ,can not start");
+    //     }else if (currentFuelInLiters < 5){
+    //     System.out.println("Car is in reserved mode , please refuel");
+       
+    //     }else{
+    //     System.out.println("car is started...bruhhhh");
+        
+    //     }
+    // }
+
+
+
+    // for this keyword
+
     public Car start(){
         if (currentFuelInLiters == 0 ){
             System.out.println("Car is out of fuel ,can not start");
         }else if (currentFuelInLiters < 5){
         System.out.println("Car is in reserved mode , please refuel");
-        currentFuelInLiters--;
+       
         }else{
         System.out.println("car is started...bruhhhh");
-        currentFuelInLiters--;
+        
         }
         return this;
     }
+
+
+
+
+
+
+
+
+
 
     public void drive(){
         currentFuelInLiters--;
--- Module_6/6.4(This_Keyword)/Driver.java
+++ Module_6/6.4(This_Keyword)/Driver.java
@@ -1,54 +1,75 @@
 public class Driver {
 
-static int minAgeForDriving =18;
+        public static void main(String[] args) {
 
-    String name;
-    String dateOfLicence;
-    int age;
+                // Car myCar = new Car();
+                // myCar.addFuel(6);
+                // myCar.drive();
+                // myCar.drive();
+                // myCar.drive();
+                // myCar.addFuel(6);
+                // myCar.drive();
+                // System.out.println(myCar.currentFuelInLiters);
 
+                // Car swift = new Car();
+                // swift.addFuel(6);
+                // swift.start();
+                // swift.drive();
 
-    public boolean isAllowedToDrive(){
-        return this.age>=minAgeForDriving;
-    }
+                // for this keyword
 
+                Car swift = new Car(); // Step 1: Creating Car object named 'swift'
+                swift.addFuel(6); // Step 2: Adding 6 liters of fuel
+                Car startedCar = swift.start(); // Step 3: Starting the car, returns 'this' (which is the same 'swift'  object)
+                startedCar.drive(); // Step 4: Calling drive() on the returned object
 
-
-    public static void main(String[] args) {
-
-
-        // Car myCar = new Car();
-        // myCar.addFuel(6);
-        // myCar.drive();
-        // myCar.drive();
-        // myCar.drive();
-        // myCar.addFuel(6);
-        // myCar.drive();
-        // System.out.println(myCar.currentFuelInLiters);
-
-        Car swift = new Car();
-        swift.addFuel(6);
-
-        // Car startedCar =  swift.start(); //car is started...bruhhhh
-        // startedCar.drive(); // car is driving
-
-        swift.start().drive(); // for method chaining
-        /*
-         * car is started...bruhhhh
-         * car is driving
-         * 
-         */
-
-
-         Driver myDriver = new Driver();
-        //  myDriver.
-
-        //  driver.
-
-        // dateOfLicenc e
-
-        System.out.println(minAgeForDriving);
-
-
-
-     } 
+        }
 }
+
+// public class Driver {
+
+// static int minAgeForDriving =18;
+
+// String name;
+// String dateOfLicence;
+// int age;
+
+// public boolean isAllowedToDrive(){
+// return this.age>=minAgeForDriving;
+// }
+
+// public static void main(String[] args) {
+
+// // Car myCar = new Car();
+// // myCar.addFuel(6);
+// // myCar.drive();
+// // myCar.drive();
+// // myCar.drive();
+// // myCar.addFuel(6);
+// // myCar.drive();
+// // System.out.println(myCar.currentFuelInLiters);
+
+// Car swift = new Car();
+// swift.addFuel(6);
+
+// // Car startedCar = swift.start(); //car is started...bruhhhh
+// // startedCar.drive(); // car is driving
+
+// swift.start().drive(); // for method chaining
+// /*
+// * car is started...bruhhhh
+// * car is driving
+// *
+// */
+
+// Driver myDriver = new Driver();
+// // myDriver.
+
+// // driver.
+
+// // dateOfLicenc e
+
+// System.out.println(minAgeForDriving);
+
+// }
+// }
